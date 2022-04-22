39c6c658062c1897e17ddbe9b09b506552ee33b2
Nikita B <nikita.balobanov@gmail.com>
2022-04-22 17:34:59 +0300

    Add vite-plugin-pages

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,17 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import vue from '@vitejs/plugin-vue';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Pages({
+      routeStyle: 'nuxt',
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
